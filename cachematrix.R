c4fee9df589afe8007659fde75427849838f41b0
rblenner <rblenner@gmail.com>
2021-02-07 10:37:46 +1100

    getting ready to push example cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# function to set / get the value of the matrix and to set / get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  #initialize the value of the inverse
+  inv <- NULL
+  #assign matrix properties to a different environment (cache)
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  ##setting / getting matrix inverse 
+  get <- function() x
+  #inverse of matrix
+  setinverse <- function() inv <<- solve(x)
+  #getting inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
+ 
 
 
-## Write a short comment describing this function
+## function to calculate the inverse of the matrix created by the function above
+  # checks to see if the inverse has already been calculated, skipping the calculation if it has
+  # sets the inverse in the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #return the inverse if already present, else, proceed to calculation
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  #compute inverse of the matrix
+  inv <- solve(data, ...)
+  x$getinverse(inv)
+  #return the inverse
+  inv
 }
+
+
+
+#brief test
+funct <- makeCacheMatrix()
+funct$set(matrix(1:4, 2))
+funct$get()
+funct$setinverse()
+funct$getinverse()
+
+
+
